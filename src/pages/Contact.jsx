715cc5ad48ba5839993a7733c10a002cdf86e414
unknown <aravindkumar40m@gmail.com>
2024-03-30 16:12:59 +0530

    contact page added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,166 @@
-import React from "react";
+import React, { Suspense, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import useAlert from "../hooks/useAlert";
+import Alert from "../components/Alert";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
+import Fox from "../models/Fox";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const formRef = useRef();
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [loading, setLoading] = useState(false);
+
+  const { showAlert, hideAlert, alert } = useAlert();
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const handleChange = ({ target: { name, value } }) => {
+    setForm((currForm) => {
+      return { ...currForm, [name]: value };
+    });
+  };
+
+  const handleFocus = () => setCurrentAnimation("walk");
+  const handleBlur = () => setCurrentAnimation("idle");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setCurrentAnimation("hit");
+    emailjs
+      .send(
+        "service_aj32ei6",
+        "template_7n6gwaj",
+        {
+          from_name: form.name,
+          to_name: "Aravindkumar",
+          from_email: form.email,
+          to_email: "aravindkumar40m@gmail.com",
+          message: form.message,
+        },
+        "1V0DpkKRK_1gEWUIn"
+      )
+      .then(() => {
+        setLoading(false);
+        showAlert({
+          show: true,
+          text: "Thanks for you message 😊",
+          type: "success",
+        });
+
+        setTimeout(() => {
+          hideAlert(false);
+          setCurrentAnimation("idle");
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        }, 3000);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+        setCurrentAnimation("idle");
+        showAlert({
+          show: true,
+          text: "I didn't receive your message 😢",
+          type: "danger",
+        });
+      });
+  };
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert} />}
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col gap-7 mt-14"
+        >
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="John"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="john@gmail.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              name="message"
+              rows="4"
+              className="input"
+              placeholder="write yours thoughts here..."
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button type="submit" className="btn" disabled={loading}>
+            {loading ? "Sending..." : "Submit"}
+          </button>
+        </form>
+      </div>
+
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000,
+          }}
+        >
+          <directionalLight position={[0, 0, 1]} intensity={2.5} />
+          <ambientLight intensity={1} />
+          <pointLight position={[5, 10, 0]} intensity={2} />
+          <spotLight
+            position={[10, 10, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
+
+          <Suspense fallback={<Loader />}>
+            <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.629, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          200: "#d5dae1",
+        },
+        black: {
+          DEFAULT: "#000",
+          500: "#1D2235",
+        },
+        blue: {
+          500: "#2b77e7",
+        },
+      },
+      fontFamily: {
+        worksans: ["work Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      boxShadow: {
+        card: "0px 1px 2px 0px rbga(0,0,0,0.05)",
+      },
+    },
   },
   plugins: [],
 };
